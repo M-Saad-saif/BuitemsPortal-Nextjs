10fb782ce459b0890979f3236642c653f24302c4
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-03-20 00:05:48 +0500

    fix 7

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -10,9 +9,9 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "https://buitems-portal-nextjs.vercel.app",       // replace with your previous link
-        destination: "https://buitems-portal.vercel.app",  // replace with your new link
-        permanent: true,           // 301 redirect
+        source: "/",
+        destination: "https://buitems-portal.vercel.app",
+        permanent: true,
       },
     ];
   },
